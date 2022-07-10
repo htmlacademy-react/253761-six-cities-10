3a02d574dbb8b7c235669d6bfd9b9c271247dc7e
mikesamotaev <samotaev.mikhail@gmail.com>
2022-07-10 17:18:07 +0300

    Pages: main, room

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+//import Room from '../../pages/room/room';
+
+type AppProps = {
+  placesFound: number,
+};
+
+function App({placesFound}: AppProps): JSX.Element {
+  return <Main placesFound={placesFound} />;
+  //return <Room />;
 }
 
 export default App;
