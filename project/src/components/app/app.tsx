ce1a8fc4c496999e092e42f97ecd2a501dcd9ac2
mikesamotaev <samotaev.mikhail@gmail.com>
2022-07-11 14:35:23 +0300

    Pages: favourites; login

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import Main from '../../pages/main/main';
 //import Room from '../../pages/room/room';
+//import Favourites from '../../pages/favourites/favourites';
+//import Login from '../../pages/login/login';
 
 type AppProps = {
   placesFound: number,
@@ -8,6 +10,8 @@ type AppProps = {
 function App({placesFound}: AppProps): JSX.Element {
   return <Main placesFound={placesFound} />;
   //return <Room />;
+  //return <Favourites />;
+  //return <Login />;
 }
 
 export default App;
