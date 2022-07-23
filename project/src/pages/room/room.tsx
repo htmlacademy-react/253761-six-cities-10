e949f83bdad7256c542370449e3f5a484f4c4130
mikesamotaev <samotaev.mikhail@gmail.com>
2022-07-23 15:56:56 +0300

    Header component

--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 
 import RoomForm from '../../components/room-form/room-form';
 import NotFound from '../not-found/not-found';
+import Header from '../../components/header/header';
 
 type RoomRtype = {
   offers: IOffer[]
@@ -26,34 +27,7 @@ function Room({ offers }: RoomRtype): JSX.Element {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--property">
         <section className="property">
